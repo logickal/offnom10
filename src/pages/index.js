1ff418c9309871b22e7e829e367f66e56fd1e59e
Jeremy Dickens <jeremy.dickens@pantheon.io>
2023-03-10 19:52:26 +0000

    Initial up

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,9 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
+       Offnom 10
       </h1>
       <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
