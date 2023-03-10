65e8c1e7c4298911ea8b2e018c8e34242f22d44a
Jeremy Dickens <jeremy.dickens@pantheon.io>
2023-03-10 19:56:11 +0000

    Initial add of Gatsby Source Drupal

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@ module.exports = {
     title: `Offnom10`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        baseUrl: `https://offnom10.com/`,
+        apiBase: `jsonapi`, // optional, defaults to `jsonapi`
+      },
+    },
+  ]
 }
