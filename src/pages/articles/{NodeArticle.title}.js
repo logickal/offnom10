d13e4c8b8fd2e3d834f22999c40e51a62bbc3e8a
Jeremy Dickens <jeremy.dickens@pantheon.io>
2023-03-14 17:09:04 -0500

    Adding images to the article template

--- src/pages/articles/{NodeArticle.title}.js
+++ src/pages/articles/{NodeArticle.title}.js
@@ -1,13 +1,15 @@
 import { graphql } from "gatsby"
 import React from "react"
+import {GatsbyImage, getImage } from "gatsby-plugin-image"
 
 const ArticleTemplate = ({ data }) => {
   const article = data.nodeArticle
+  const image = getImage(article.relationships.field_image.localFile)
   return (
     <div>
       <h1>{article.title}</h1>
       <p>Published on {article.created}</p>
-
+      <GatsbyImage image={image} alt={article.title} />
       <div dangerouslySetInnerHTML={{ __html: article.body.processed }} />
     </div>
   )
@@ -17,13 +19,27 @@ export default ArticleTemplate
 
 export const query = graphql`
     query($id: String!) {
-        nodeArticle(id: {eq: $id}) {
-            id
-            title
-            created(formatString: "MMMM DD, YYYY")
-            body {
-                processed
-            }
+      nodeArticle(id: {eq: $id}, relationships: {field_image: {}}) {
+        id
+        title
+        created(formatString: "MMMM DD, YYYY")
+        body {
+          processed
         }
+        relationships {
+          field_image {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(
+                  width: 600
+                  height: 600
+                  placeholder: BLURRED
+                  formats: [AUTO, WEBP, AVIF]
+                )
+              }
+            }
+          }
+        }
+      }
     }
 `
