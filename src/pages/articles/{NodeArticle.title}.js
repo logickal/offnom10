f8b0c4993f951866b95a18b443239c49f204613b
Jeremy Dickens <jeremy.dickens@pantheon.io>
2023-03-15 17:09:25 -0500

    Add Gif handling to NodeArticle

--- src/pages/articles/{NodeArticle.title}.js
+++ src/pages/articles/{NodeArticle.title}.js
@@ -5,11 +5,19 @@ import {GatsbyImage, getImage } from "gatsby-plugin-image"
 const ArticleTemplate = ({ data }) => {
   const article = data.nodeArticle
   const image = getImage(article.relationships.field_image.localFile)
+  let imageTag
+  if (article.relationships.field_image.localFile.extension === 'gif') {
+    imageTag = <img src={article.relationships.field_image.localFile.publicURL} alt={article.title} />
+
+  } else {
+    imageTag = <GatsbyImage image={image} alt={article.title} />
+  }
+
   return (
     <div>
       <h1>{article.title}</h1>
       <p>Published on {article.created}</p>
-      <GatsbyImage image={image} alt={article.title} />
+      {imageTag}
       <div dangerouslySetInnerHTML={{ __html: article.body.processed }} />
     </div>
   )
@@ -29,6 +37,8 @@ export const query = graphql`
         relationships {
           field_image {
             localFile {
+              extension
+              publicURL
               childImageSharp {
                 gatsbyImageData(
                   width: 600
